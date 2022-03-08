265b4c0263fa0d75c2fbb99d17d501aa44a4db4d
Sergi Puerto <sergipuerto@gmail.com>
2022-03-08 10:36:39 +0100

    awaits

--- index2.js
+++ index2.js
@@ -3,37 +3,40 @@ const { ipcMain, dialog } = require('electron');
 const fs = require('fs');
 const jszip = require('jszip');
 
-ipcMain.on('selectDir', async(even, args) => {
+ipcMain.on('selectDir', async (even, args) => {
     const result = await dialog.showOpenDialog({
         properties: ['openDirectory']
     });
     even.reply('selectDir', result.filePaths[0])
 });
 
-ipcMain.on('zipPath', async(even, args)=>{
+ipcMain.on('zipPath', async (even, args) => {
     let contadorDePdf = 0;
-    fs.readdir(args, function(err, filenames){
-        filenames.forEach(function(filename){
-            if(filename.endsWith('.zip')){
-                fs.readFile(args +"\\"+ filename, function(err, data){
-                    jszip.loadAsync(data)
-                        .then(function(zip){
-                            zip.forEach(function (relativePath, zipEntry) {  // 2) print entries
-                                if(zipEntry.name.endsWith('pdf')){
-                                    zipEntry.async("nodebuffer")
-                                        .then(function(data){
-                                            let nameFile = filename.substring(0, filename.lastIndexOf('.')) + '.pdf';
-                                            fs.writeFileSync(args+'\\'+ nameFile, data);
-                                            contadorDePdf++;
-                                            even.reply('zipPath', "Zip nº " + contadorDePdf + '. Archivo ' + filename + ' extrae ' + nameFile);
-                                        });
+    fs.readdir(args, function (err, filenames) {
+        if (err) {
+            even.reply('zipPath', "Error en la lectura de la carpeta.");
+        } else {
+            for (let i = 0; i < filenames.length; i++) {
+                let filename = filenames[i];
+                if (filename.endsWith('.zip')) {
+                    fs.readFile(args + '\\' + filename, async (err, data) => {
+                        if (err) {
+                            even.reply('zipPath', filename + ' está corrupto.')
+                        } else {
+                            let zip = await jszip.loadAsync(data);
+                            zip.forEach(async function(relativePath, zipEntry) {
+                                if (zipEntry.name.endsWith('pdf')) {
+                                    let buffer = await zipEntry.async('nodebuffer');
+                                    let nameFile = filename.substring(0, filename.lastIndexOf('.')) + '.pdf';
+                                    fs.writeFileSync(args + '\\' + nameFile, buffer);
+                                    contadorDePdf++;
+                                    even.reply('zipPath', "Zip nº " + contadorDePdf + '. Archivo ' + filename + ' extrae ' + nameFile);
                                 }
-                            });
-                            
-                        });
-                });
-            }
-        });
-    });
+                            })
+                        }
+                    })
+                }
+            };
+        }
+    })
 })
-
